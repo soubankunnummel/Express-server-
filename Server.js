cf3a7c8d035a20901fca6f500b1badf8202f2819
unknown <muhammedsoubabkunnummek123@gmail.com>
2023-10-28 09:14:00 +0530

    first commit

---
+++ Server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send("Home page")
+
+})
+
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+})
